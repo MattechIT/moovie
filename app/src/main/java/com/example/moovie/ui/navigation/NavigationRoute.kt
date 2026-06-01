462bbae45bafcf84bb87cd2aa7cea73bd999622d
MattechIT <matteo.todeschi2@studio.unibo.it>
2026-06-01 16:14:12 +0200

    Add NavigationRoute sealed interface for app navigation

---
+++ app/src/main/java/com/example/moovie/ui/navigation/NavigationRoute.kt
@@ -0,0 +1,42 @@
+package com.example.moovie.ui.navigation
+
+import kotlinx.serialization.Serializable
+
+sealed interface NavigationRoute {
+    
+    @Serializable
+    data object Splash : NavigationRoute
+    
+    @Serializable
+    data object Login : NavigationRoute
+    
+    @Serializable
+    data object Register : NavigationRoute
+    
+    @Serializable
+    data object Home : NavigationRoute
+    
+    @Serializable
+    data object Search : NavigationRoute
+    
+    @Serializable
+    data object Watchlist : NavigationRoute
+    
+    @Serializable
+    data object Favorites : NavigationRoute
+    
+    @Serializable
+    data object Profile : NavigationRoute
+    
+    @Serializable
+    data object Settings : NavigationRoute
+    
+    @Serializable
+    data object Stats : NavigationRoute
+    
+    @Serializable
+    data class Detail(val movieId: Int) : NavigationRoute
+    
+    @Serializable
+    data object MovieExplorer : NavigationRoute
+}
