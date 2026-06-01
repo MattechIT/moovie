2d4d2c6312c8f63396e90222dfc297b777a50efe
MattechIT <matteo.todeschi2@studio.unibo.it>
2026-06-01 16:47:56 +0200

    Add AuthViewModel to app module

--- app/src/main/java/com/example/moovie/di/AppModule.kt
+++ app/src/main/java/com/example/moovie/di/AppModule.kt
@@ -2,9 +2,14 @@ package com.example.moovie.di
 
 import com.example.moovie.data.repository.AuthRepository
 import com.example.moovie.data.repository.MockAuthRepository
+import com.example.moovie.presentation.auth.AuthViewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
     // Auth repository implementation
     single<AuthRepository> { MockAuthRepository() }
+    
+    // Auth ViewModel
+    viewModel { AuthViewModel(get()) }
 }
