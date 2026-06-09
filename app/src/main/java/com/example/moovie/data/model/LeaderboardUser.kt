43a873cf13553694216839851e0e3df934a57a6f
MattechIT <matteo.todeschi2@studio.unibo.it>
2026-06-09 11:08:59 +0200

    Created leatherboard model

---
+++ app/src/main/java/com/example/moovie/data/model/LeaderboardUser.kt
@@ -0,0 +1,11 @@
+package com.example.moovie.data.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class LeaderboardUser(
+    val id: String,
+    val username: String? = null,
+    val avatar_url: String? = null,
+    val movies_count: Int = 0
+)
